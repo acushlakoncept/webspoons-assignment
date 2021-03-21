a568e5730b47b97f0fcadf561a47a4b150f99b18
Uduak Essien <acushla4real@gmail.com>
2021-03-21 23:16:36 +0100

    Add seed for 1000 users and update user index styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+1000.times do
+    status = rand(0..1) == 1 ? 'active' : 'inactive'
+    User.create(title: Faker::Name.prefix, name: Faker::Name.unique.name, email: Faker::Internet.unique.email,
+                phone: Faker::PhoneNumber.cell_phone_in_e164.gsub('+', ''), status: status)
+  end

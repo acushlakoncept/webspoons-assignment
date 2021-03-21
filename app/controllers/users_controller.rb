453bc79f67278dc156e641248168a99a3c38d27a
Uduak Essien <acushla4real@gmail.com>
2021-03-21 22:16:01 +0100

    Add user controller with all actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,62 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 25).order_users
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_url, notice: 'User was successfully created.' }
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace('user_form',
+                                                    partial: 'users/form',
+                                                    locals: { user: @user })
+        end
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_url, notice: 'User was successfully updated.' }
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace('user_form',
+                                                    partial: 'users/form',
+                                                    locals: { user: @user })
+        end
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :title, :phone, :status)
+  end
+end

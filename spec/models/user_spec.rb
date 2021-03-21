a0eb2e2d557ff2e3eb5eb96067054dcb0f01a9b9
Uduak Essien <acushla4real@gmail.com>
2021-03-21 23:31:12 +0100

    Add unit testing for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    # Validation
+    it { should validate_presence_of(:title) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(2) }
+
+    it { should validate_presence_of(:phone) }
+
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+
+    it { should allow_values(:active, :inactive).for(:status) }
 end

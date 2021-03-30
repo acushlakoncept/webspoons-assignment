34e56054ebe2355fa5a0f12c2b773917ac58917f
Uduak Essien <acushla4real@gmail.com>
2021-03-30 16:43:41 +0100

    Update user_spec.rb

--- spec/feature/user_spec.rb
+++ spec/feature/user_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe User, type: :feature do
     visit root_path
     expect(page).to have_content 'All Users'
   end
+  
+    it 'loads new user modal form' do
+    visit root_path
+    click_link 'New User'
+    within('[data-user-modal-target="container"]') do
+      expect(page).to have_content 'Create User'
+    end
+  end
+
+  it 'adds a new user' do
+    visit root_path
+    click_link 'New User'
+    within('[data-user-modal-target="container"]') do
+      fill_in 'user[name]', with: 'Test User'
+      fill_in 'user[email]', with: 'test.user@mail.com'
+      fill_in 'user[phone]', with: '11111111111'
+      click_button('Create User')
+    end
+    expect(page).to have_content('User was successfully created.')
+  end
 end
+
